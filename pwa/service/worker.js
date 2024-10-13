2c25a7b0f9162aff3c57db821256271672ccdbeb
zero72542 <mdrakibmiafb@gmail.com>
2024-10-13 10:34:12 +0600

    Update worker.js

--- pwa/service/worker.js
+++ pwa/service/worker.js
@@ -1,14 +1,12 @@
 const OFFLINE_VERSION = 'v1';
 const CACHE_NAME = 'offline';
 const OFFLINE_URL = '@page.html?path=pwa/offline';
-
 self.addEventListener('install', (event) => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
   })());
 });
-
 self.addEventListener('activate', (event) => {
   event.waitUntil((async () => {
     if ('navigationPreload' in self.registration){
@@ -17,14 +15,12 @@ self.addEventListener('activate', (event) => {
   })());
   self.clients.claim();
 });
-
-// Handle offline fetch
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
+  if (event.request.mode === 'navigate'){
     event.respondWith((async () => {
       try {
         const preloadResponse = await event.preloadResponse;
-        if (preloadResponse) {
+        if (preloadResponse){
           return preloadResponse;
         }
         const networkResponse = await fetch(event.request);
@@ -36,38 +32,4 @@ self.addEventListener('fetch', (event) => {
       }
     })());
   }
-});
-
-// Handle push event for notifications
-self.addEventListener('push', function(event) {
-  const data = event.data.json();
-
-  const options = {
-    body: data.body,
-    icon: data.icon,
-    data: { url: data.url } // Pass post URL for notification click
-  };
-
-  event.waitUntil(
-    self.registration.showNotification(data.title, options)
-  );
-});
-
-// Handle notification click event
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  const postUrl = event.notification.data.url;
-
-  event.waitUntil(
-    clients.matchAll({ type: 'window' }).then(windowClients => {
-      for (let client of windowClients) {
-        if (client.url === postUrl && 'focus' in client) {
-          return client.focus();
-        }
-      }
-      if (clients.openWindow) {
-        return clients.openWindow(postUrl);
-      }
-    })
-  );
 });
